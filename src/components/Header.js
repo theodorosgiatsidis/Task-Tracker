77518cecc6380ab2fce6635d0765d17d261f03cf
Thodoros Giatsidis <46629765+theodorosgiatsidis@users.noreply.github.com>
2021-12-02 15:36:13 +0200

    useLocation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
 function Header({ title, onAdd, showAdd }) {
+  const location = useLocation();
   return (
     <div>
       <header className="header">
         <h1>{title}</h1>
-        <Button
-          onClick={onAdd}
-          color={showAdd ? "red" : "green"}
-          text={showAdd ? "Close" : "Add"}
-        />
+        {location.pathname === "/" && (
+          <Button
+            onClick={onAdd}
+            color={showAdd ? "red" : "green"}
+            text={showAdd ? "Close" : "Add"}
+          />
+        )}
       </header>
     </div>
   );
